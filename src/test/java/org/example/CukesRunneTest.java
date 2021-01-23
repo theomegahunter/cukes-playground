cf539831c9f410fa102911fa03d45b5f0491552c
Nathan Schulke <zeus.omega@gmail.com>
2021-01-23 12:51:34 -0500

    Refining and making the cucumber tests run during test phase

---
+++ src/test/java/org/example/CukesRunneTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/it/features/", glue = "")
+public class CukesRunneTest {
+
+}
--- src/test/java/org/example/GlueCode.java
+++ src/test/java/org/example/GlueCode.java
@@ -1,26 +1,26 @@
 package org.example;
 
+
 import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
 public class GlueCode {
+    int bananaPrice = -1;
 
     @Given("^the price of a \"(.*?)\" is (\\d+)c$")
-    public void thePriceOfAIsC(String arg1, int arg2) throws Throwable {
-        throw new PendingException();
+    public void thePriceOfAIsC(String name, int price) throws PendingException {
+        bananaPrice = price;
     }
 
-    @When("^I checkout(\\d+)\"(.*?)\"$")
-    public void iCheckout(int arg1, String arg2) throws Throwable {
+    @When("^I checkout {int} {string}$")
+    public void iCheckout(int arg1, String arg2) throws PendingException {
         throw new PendingException();
     }
 
     @Then("^the total price should be (\\d+)c$")
-    public void theTotalPriceShouldBeC(int arg1) throws Throwable {
+    public void theTotalPriceShouldBeC(int arg1) throws PendingException {
         throw new PendingException();
     }
 }
